5c09558afc1ea04ece12c6bd3c911f32f9e184e8
MeghlaB <meghlab964@gmail.com>
2024-12-23 19:29:38 +0600

    change Layout

--- src/Pages/LatestFindLostItems.jsx
+++ src/Pages/LatestFindLostItems.jsx
@@ -1,90 +1,92 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import { FaSearch } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import { Typewriter } from 'react-simple-typewriter';
 
 const LatestFindLostItems = () => {
-  const [items, setItems] = useState([]);
-  const [searchQuery, setSearchQuery] = useState('');
-useEffect(()=>{
-axios.get(`${import.meta.env.VITE_API_URL}/allItems`)
-.then(res=>{
-    setItems(res.data)
-})
-},[])
+    const [items, setItems] = useState([]);
+    const [searchQuery, setSearchQuery] = useState('');
+    useEffect(() => {
+        axios.get(`${import.meta.env.VITE_API_URL}/allItems`)
+            .then(res => {
+                setItems(res.data)
+            })
+    }, [])
 
-const filteredItems = items.filter(
-    item =>
-      item.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
-      item.location.toLowerCase().includes(searchQuery.toLowerCase())
-  );
-
-
+    const filteredItems = items.filter(
+        item =>
+            item.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+            item.location.toLowerCase().includes(searchQuery.toLowerCase())
+    );
 
 
 
-  return (
-    <div className="max-w-7xl mx-auto p-4">
-    <h2 className="text-center text-2xl text-purple-600 font-bold mb-6"> 
-        <Typewriter
-            words={['Lost & Found Items']}
-            loop={Infinity}
-            cursor
-            cursorStyle='_'
-            typeSpeed={50}
-            deleteSpeed={50}
-            delaySpeed={1000}
-        /></h2>
 
-       {/* Search Input */}
-       <div className="mb-6">
-        <input
-          type="text"
-          placeholder="Search by title or location..."
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-          className="input input-bordered w-full md:w-96"
-        />
-      </div>
 
-    <div className="pt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
-    {filteredItems.length > 0 ? (
-          filteredItems.map((item) => (
-            <div key={item._id} className="card card-compact bg-base-100 w-96 shadow-xl m-2">
-              <figure>
-                <img
-                  src={item.thumbnail}
-                  alt="thumbnail"
-                  className="w-full h-48 object-cover"
-                />
-              </figure>
-              <div className="card-body">
-                <h2 className="card-title">{item.title}</h2>
-                <p className="text-xl text-gray-600">Location: {item.location}</p>
-                <p className="text-xl text-gray-600">
-                  Date: {new Date(item.dateLost).toLocaleDateString()}
-                </p>
-                <div className="card-actions justify-end">
-                  <Link to={`/details/${item._id}`}>
-                    <button className="btn bg-purple-600 text-white">Details</button>
-                  </Link>
-                </div>
-              </div>
+    return (
+        <div className="max-w-7xl mx-auto p-4">
+            <h2 className="text-center text-2xl text-purple-600 font-bold mb-6">
+                <Typewriter
+                    words={['Lost & Found Items']}
+                    loop={Infinity}
+                    cursor
+                    cursorStyle='_'
+                    typeSpeed={50}
+                    deleteSpeed={50}
+                    delaySpeed={1000}
+                /></h2>
+
+            {/* Search Input */}
+            <div className="mb-6">
+                <label className="input border-2 border-purple-600  input-bordered flex items-center gap-2">
+                    <input type="text"
+                    value={searchQuery} 
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                    className="grow"
+                     placeholder="Search by title or location..." />
+                    <FaSearch />
+                </label>
             </div>
-          ))
-        ) : (
-          <p className="text-center text-lg text-gray-500">No items found.</p>
-        )}
-    </div>
-    <div className="flex justify-center items-center mt-6">
-  <Link to="/allItems" className="btn px-6 bg-purple-600 text-white">
-    See All
-  </Link>
-</div>
-  </div>
-  
 
-  );
+            <div className="pt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+                {filteredItems.length > 0 ? (
+                    filteredItems.map((item) => (
+                        <div key={item._id} className="card card-compact bg-base-100 w-96 shadow-xl m-2">
+                            <figure>
+                                <img
+                                    src={item.thumbnail}
+                                    alt="thumbnail"
+                                    className="w-full h-48 object-cover"
+                                />
+                            </figure>
+                            <div className="card-body">
+                                <h2 className="card-title">{item.title}</h2>
+                                <p className="text-xl text-gray-600">Location: {item.location}</p>
+                                <p className="text-xl text-gray-600">
+                                    Date: {new Date(item.dateLost).toLocaleDateString()}
+                                </p>
+                                <div className="card-actions justify-end">
+                                    <Link to={`/details/${item._id}`}>
+                                        <button className="btn bg-purple-600 text-white">Details</button>
+                                    </Link>
+                                </div>
+                            </div>
+                        </div>
+                    ))
+                ) : (
+                    <p className="text-center text-lg text-gray-500">No items found.</p>
+                )}
+            </div>
+            <div className="flex justify-center items-center mt-6">
+                <Link to="/allItems" className="btn px-6 bg-purple-600 text-white">
+                    See All
+                </Link>
+            </div>
+        </div>
+
+
+    );
 };
 
 export default LatestFindLostItems;
--- src/PrivetRoute/Allrecovery.jsx
+++ src/PrivetRoute/Allrecovery.jsx
@@ -1,46 +1,85 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { AuthContext } from '../AuthProvider/AuthProvider'
 import axios from 'axios'
+import { TfiLayoutGrid3Alt } from 'react-icons/tfi'
+import { CiViewTable } from 'react-icons/ci'
+import { Typewriter } from 'react-simple-typewriter'
 
 export default function Allrecovery() {
-const {user,loading,setLoading} = useContext(AuthContext)
-const [recoveredItems , setIsRecovered] = useState([])
+    const { user, loading, setLoading } = useContext(AuthContext)
+    const [recoveredItems, setIsRecovered] = useState([])
+    const [iscardLayOut, setLayOut] = useState(false)
 
-useEffect(()=>{
-    axios.get(`${import.meta.env.VITE_API_URL}/allrcoveries`)
-    .then(res=>{
-        setIsRecovered(res.data)
-    })
-},[])
-  return (
-    <div className="container mx-auto p-6">
-      <h2 className="text-3xl font-bold text-center mb-6">All Recovered Items</h2>
-      {recoveredItems.length === 0 ? (
-        <div className="text-center text-xl text-gray-600">
-          No recovered items found. You haven't recovered any items yet.
+
+    useEffect(() => {
+        axios.get(`${import.meta.env.VITE_API_URL}/allrcoveries`)
+            .then(res => {
+                setIsRecovered(res.data)
+            })
+    }, [])
+    return (
+        <div className="container mx-auto p-6">
+            <h2 className="text-3xl font-bold text-center mb-6">
+                <Typewriter
+                    words={['All Recovered Items']}
+                    loop={Infinity}
+                    cursor
+                    cursorStyle={<span style={{ color: 'purple', fontSize: '30px' }}>|</span>}
+                    typeSpeed={50}
+                    deleteSpeed={50}
+                    delaySpeed={1000}
+                />
+
+                </h2>
+            <div className='text-right mb-4'>
+                <button
+                    className=" bg-purple-600 px-4 py-2 text-3xl text-white rounded"
+                    onClick={() => setLayOut(!iscardLayOut)}
+                >
+                    {
+                        iscardLayOut ? <TfiLayoutGrid3Alt /> : <CiViewTable />
+                    }
+                </button>
+            </div>
+
+            {recoveredItems.length === 0 ? (
+                <div className="text-center text-xl text-gray-600">
+                    No recovered items found. You haven't recovered any items yet.
+                </div>
+            ) : iscardLayOut ? (
+                // Card Layout
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                    {recoveredItems.map((item) => (
+                        <div key={item._id} className="border border-purple-500 border-1  p-4 rounded shadow hover:shadow-lg">
+                            <h3 className="text-lg font-bold">{item.title}</h3>
+                            <p><strong>Category:</strong> {item.category}</p>
+                            <p><strong>Recovered Location:</strong> {item.recoverLocation}</p>
+                            <p><strong>Recovered Date:</strong> {new Date(item.recoveryDate).toLocaleDateString()}</p>
+                        </div>
+                    ))}
+                </div>
+            ) : (
+                <table className="min-w-full table-auto border-collapse border border-gray-200">
+                    <thead>
+                        <tr className="bg-purple-600 text-white">
+                            <th className="px-4 py-2">Title</th>
+                            <th className="px-4 py-2">Category</th>
+                            <th className="px-4 py-2">Recovered Location</th>
+                            <th className="px-4 py-2">Recovered Date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {recoveredItems.map((item) => (
+                            <tr key={item._id} className="border-b hover:bg-gray-50">
+                                <td className="px-4 py-2">{item.title}</td>
+                                <td className="px-4 py-2">{item.category}</td>
+                                <td className="px-4 py-2">{item.recoverLocation}</td>
+                                <td className="px-4 py-2">{new Date(item.recoveryDate).toLocaleDateString()}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            )}
         </div>
-      ) : (
-        <table className="min-w-full table-auto border-collapse border border-gray-200">
-          <thead>
-            <tr className="bg-purple-600 text-white">
-              <th className="px-4 py-2">Title</th>
-              <th className="px-4 py-2">Category</th>
-              <th className="px-4 py-2">Recovered Location</th>
-              <th className="px-4 py-2">Recovered Date</th>
-            </tr>
-          </thead>
-          <tbody>
-            {recoveredItems.map((item) => (
-              <tr key={item._id} className="border-b hover:bg-gray-50">
-                <td className="px-4 py-2">{item.title}</td>
-                <td className="px-4 py-2">{item.category}</td>
-                <td className="px-4 py-2">{item.recoverLocation}</td>
-                <td className="px-4 py-2">{new Date(item.recoveryDate).toLocaleDateString()}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
-    </div>
-  )
+    )
 }
